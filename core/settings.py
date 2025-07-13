759650e0c8ed14ec9ecc9557b19b6cc4eda4d13c
UbongPr7 <ubongpr7@gmail.com>
2025-07-13 14:25:01 +0100

    server setup for microservice

--- core/settings.py
+++ core/settings.py
@@ -271,6 +271,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://intera-inventory.vercel.app",
     "http://3.84.22.207:3000",
     'https://agentic-caller-gvlu.onrender.com',
+    'https://intera-inventory.vercel.app'
 
 ]
 
