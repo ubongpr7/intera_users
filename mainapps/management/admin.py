a64edc841f59a92b480543d8996ea655cb1c5d23
UbongPr7 <ubongpr7@gmail.com>
2025-07-25 00:20:23 +0100

    server setup for microservice

--- mainapps/management/admin.py
+++ mainapps/management/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mainapps.utils.registrar import register_models,ProfileAgent
+from mainapps.utils.registrar import register_models
 from .models import *
 register_models(registerable_models)
 
